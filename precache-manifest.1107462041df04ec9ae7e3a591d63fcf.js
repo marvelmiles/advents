5e0461168c569dc1c6d884164aeea13dd9f8b4df
Akinrinmola marvellous <marvellousabidemi2@gmail.com>
2020-07-24 20:45:19 +0100

    Updates

---
+++ precache-manifest.1107462041df04ec9ae7e3a591d63fcf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f5e7a33a4c5a880a81fe0204ce05e1",
+    "url": "/advents/index.html"
+  },
+  {
+    "revision": "6fda48c6f7fa276700d6",
+    "url": "/advents/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "37b9201f3d6191af28f6",
+    "url": "/advents/static/css/main.cf4ae164.chunk.css"
+  },
+  {
+    "revision": "6fda48c6f7fa276700d6",
+    "url": "/advents/static/js/2.6bf544b9.chunk.js"
+  },
+  {
+    "revision": "deeb39eb1c82ccefce09873c2cfbd835",
+    "url": "/advents/static/js/2.6bf544b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b9201f3d6191af28f6",
+    "url": "/advents/static/js/main.e557aa51.chunk.js"
+  },
+  {
+    "revision": "795892f880edd3378230",
+    "url": "/advents/static/js/runtime-main.7751c276.js"
+  },
+  {
+    "revision": "ba45c8f60456a672e003a875e469d0eb",
+    "url": "/advents/static/media/Desert.ba45c8f6.jpg"
+  },
+  {
+    "revision": "c12a40bb9970dabd338d578083574cfe",
+    "url": "/advents/static/media/amazon_logo.c12a40bb.png"
+  },
+  {
+    "revision": "e5ca374b1aecab5bb009d71da6239757",
+    "url": "/advents/static/media/details.e5ca374b.jpg"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/advents/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/advents/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/advents/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/advents/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/advents/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/advents/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/advents/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/advents/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/advents/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/advents/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/advents/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/advents/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/advents/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/advents/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/advents/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "a1e14a62efe92e91750f0ad96aae3d1c",
+    "url": "/advents/static/media/hero.a1e14a62.jpg"
+  },
+  {
+    "revision": "18308b4adbec6b64f2c4e2a1a15e3a51",
+    "url": "/advents/static/media/pricing_image.18308b4a.jpg"
+  }
+]);
